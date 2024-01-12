215914a34f911237574737821728dc04132915bd
jorge candon mora <jorgecamo21@gmail.com>
2024-01-12 17:39:13 +0100

    v3

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -1,11 +1,12 @@
 <html>
-<head>
-<title>
-@yield('titulo')
-</title>
-</head>
-<body>
-@include('partials.nav')
-@yield('contenido')
-</body>
+    <head>
+        <title>
+            @yield('titulo')
+        </title>
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])     
+       </head>
+        <body>
+            @include('partials.nav')
+            @yield('contenido')
+    </body>
 </html>
