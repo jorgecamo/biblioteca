c2f65a69766303fd11e3ba7a17b3c10f0efe5074
jorge candon mora <jorgecamo21@gmail.com>
2024-01-12 16:44:12 +0100

    v2

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,33 @@ Route::get('tabla/{id?}', function ($id=2) {
         echo "<br>";
     }
 })->where('id', "[0-9]+");
+
+Route::get('/hola', function () {
+    $nombre = 'jorge';
+    return view('hola')->with('nombre',$nombre);
+});
+
+Route::get('/adios', function () {
+    $nombre = 'jorge';
+    return view('adios',['nombre' => $nombre]);
+});
+
+Route::get('/inicio/{nombre?}', function ($nombre='manolo') {
+
+    return view('inicio',compact('nombre'));
+})->name('inicio');
+
+
+Route::get('listado', function() {
+    $libros = array(
+    array("titulo" => "El juego de Ender",
+    "autor" => "Orson Scott Card"),
+    array("titulo" => "La tabla de Flandes",
+    "autor" => "Arturo Pérez Reverte"),
+    array("titulo" => "La historia interminable",
+    "autor" => "Michael Ende"),
+    array("titulo" => "El Señor de los Anillos",
+    "autor" => "J.R.R. Tolkien")
+    );
+    return view('listado', compact('libros'));
+    })->name('listado_libros');
